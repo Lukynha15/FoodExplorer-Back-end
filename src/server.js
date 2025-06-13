744e1d1d95e13d3e39b92ee40065cde15aa0f33f
Lukynha15 <Lukynha48@gmail.com>
2025-06-13 14:14:21 -0300

    atualizações no server

--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const PORT = 3333;
+const PORT = 3000;
 require("express-async-errors");
 
 const AppError = require("./utils/AppError");
 const routes = require("./routes");
 
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use(routes);
-app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173/"],
-  credentials: true
-}));
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
